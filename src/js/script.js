0f399423e15ae2a57a2f65444f7f0aef565f69f9
Valentin <primarh111@mail.ru>
2019-12-17 13:26:55 +0300

    Добавлен слайдер (слик)

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.carusel__inner').slick({
+        infinite: true,
+        prevArrow: '<button type="button" class="slick-prev"> <img src="../img/icons/chevron-left-solid.png" alt="chevron-left"></button>',
+        nextArrow: '<button type="button" class="slick-next"> <img src="../img/icons/chevron-right-solid.png" alt="chevron-right"></button>'
+      });
+  });
+
